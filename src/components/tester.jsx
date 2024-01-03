731cdea90152e82d5f748befb8aaa2d1fa914322
rubencoda2310 <rubencoda2310@gmail.com>
2024-01-03 10:25:18 +0700

    coba coba

---
+++ src/components/tester.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+class Tester extends React.Component {
+
+    render(){
+        return (
+            <div>
+                <h1 className="red-text">Menuju React 2024</h1>
+            </div>
+        )
+    }
+
+}
+
+export default Tester
---
+++ src/components/testing.jsx
@@ -0,0 +1,14 @@
+/* eslint-disable react/prop-types */
+import '../App.css'
+
+function Testing(props) {
+    console.log(props.pacar);
+
+  return (
+    <div>
+      <h1>bismillah</h1>
+    </div>
+  )
+}
+
+export default Testing
